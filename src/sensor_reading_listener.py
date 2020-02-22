a53539da33a90d1f73ce1f4dad60d704b642744e
tianpeng <tzhang@g.harvard.edu>
2020-02-22 08:47:21 -0500

    Retrieve user input arguments in spin_and_collect script. Write documentation for spin_and_collect

--- src/sensor_reading_listener.py
+++ src/sensor_reading_listener.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Float32MultiArray
+import sys
 
 def log_light(data):
 	rospy.loginfo(data.data)
 
-def light_listener(callback):
+def light_listener(callback,robot_namespace=''):
 	print('Initializing light_listener')
 	rospy.init_node('light_listener',anonymous=True)
-	rospy.Subscriber("/turtlebot3/sensor_readings", Float32MultiArray, callback)
+	if not robot_namespace=='':
+		rospy.Subscriber("{}/sensor_readings".format(robot_namespace), Float32MultiArray, callback)
+	else:
+		rospy.Subscriber("sensor_readings", Float32MultiArray, callback)
 	rospy.spin()
 
 if __name__=='__main__':
-	light_listener(log_light)
+
+	# Get the robot name passed in by the user
+	arguments = len(sys.argv) - 1
+	position = 1
+	robot_namespace=''
+	if arguments>=position:
+		robot_namespace='/'+sys.argv[position]
+
+	light_listener(log_light,robot_namespace)
--- src/spin_and_collect.py
+++ src/spin_and_collect.py
@@ -1,28 +1,70 @@
 #!/usr/bin/env python
 import rospy
-# from std_msgs.msg import Twist
 from geometry_msgs.msg import Twist
-from sensor_reading_listener import light_listener
 from std_msgs.msg import Float32MultiArray
 import numpy as np
-
+import sys
 
 class spin_and_collect(object):
-	"""docstring for spin_and_collect"""
-	def __init__(self,pub):
-		self.pub=pub
-		self.storage=[]
+	"""The object that controls a turtlebot to spin w.r.t. its own axis and collect light sensor readings."""
+	def __init__(self,awake_freq=10):
+		self.awake_freq_=awake_freq
+		self.pub_=None
+		self.reading_records=[]
+		self.node_init_=False
+		self.spin_angular_vel_=2
+
+	# Public methods
+	# Each spin_and_collect object contains its one and only node.
+	def init_node(self):
+		if self.node_init_:
+			print("The spin_and_collect node has already been initialized.")
+			return
+		# Initialize a ROS node that handles spin and collect.
+		rospy.init_node('spin_and_collect',anonymous=True)
+		self.node_init_=True
+
+	def spin_and_collect(self,robot_namespace='',total_time=0):# By default rotate for 0s.
+		if not self.node_init_:
+			print("The spin_and_collect node is not initialized yet. Call self.init_node() first.")
+			return
+
+		# Initialize the cmd_vel publisher and clock.
+		r = rospy.Rate(self.awake_freq_)
+		print('Spin the robot and collect readings')
+		if robot_namespace!='':
+			self.pub_=rospy.Publisher('/{}/cmd_vel'.format(robot_namespace),Twist,queue_size=10)
+		else:
+			self.pub_=rospy.Publisher('cmd_vel',Twist,queue_size=10)
+
+		# Initialize the light sensor reading listener.
+		self.collect_start_(robot_namespace)
+
+		counter=0 # Keep spinning for total_time
+
+		while not rospy.is_shutdown() and counter<total_time*self.awake_freq_:
+				self.spin_()
+				counter+=1
+				r.sleep()
+
+		# Elegantly stop the robot.
+		self.stop_()
+		self.pub_=None
 		
-	def callback(self,data):
+	# Private methods
+	def callback_(self,data):
 		print(np.array(data.data))
-		self.storage.append(list(data.data))
+		self.reading_records.append(list(data.data))
 
-	def collect_start(self):
+	def collect_start_(self,robot_namespace=''):
 		print('Initializing light_listener')
-		rospy.Subscriber("/turtlebot3/sensor_readings", Float32MultiArray, self.callback)
+		if robot_namespace!='':
+			rospy.Subscriber("/{}/sensor_readings".format(robot_namespace), Float32MultiArray, self.callback_)
+		else:
+			rospy.Subscriber("sensor_readings", Float32MultiArray, self.callback_)
 		
 
-	def spin(self):
+	def spin_(self):
 		
 		twist = Twist()
 		twist.linear.x = 0.0
@@ -30,11 +72,11 @@ class spin_and_collect(object):
 		twist.linear.z = 0.0
 		twist.angular.x = 0.0
 		twist.angular.y = 0.0
-		twist.angular.z = 2
+		twist.angular.z = self.spin_angular_vel_
 		self.pub.publish(twist)
 		# print(twist)
 		
-	def stop(self):
+	def stop_(self):
 		twist = Twist()
 		twist.linear.x = 0.0
 		twist.linear.y = 0.0
@@ -46,30 +88,27 @@ class spin_and_collect(object):
 
 
 if __name__=='__main__':
-	total_time=30
+	arguments = len(sys.argv) - 1
+
+	position = 1
+	# Get the robot name passed in by the user
+	robot_namespace=''
+	if arguments>=position:
+		robot_namespace='/'+sys.argv[position]
+	
+	position=2
+	# Get the total time of spinning
+	total_time=0
+	if arguments>=position:
+		total_time=sys.argv[position]
+	
+
+
 	awake_freq=10
 
-	robot_name='/turtlebot3'
-	rospy.init_node('spin_and_collect',anonymous=True)
+	sc=spin_and_collect(awake_freq)
+	sc.init_node()	
+	sc.spin_and_collect(robot_namespace,total_time)
 	
-	# First spin, then collect
-	r = rospy.Rate(awake_freq)
-	print('Spin the robot and collect readings')
-	pub=rospy.Publisher('{}/cmd_vel'.format(robot_name),Twist,queue_size=10)
-	
-	sc=spin_and_collect(pub)
-
-	
-	sc.collect_start()
-
-	counter=0 # Keep spinning for total_time
-
-	while not rospy.is_shutdown() and counter<total_time*awake_freq:
-			sc.spin()
-			counter+=1
-			r.sleep()
-
-	# Elegantly stop the robot.
-	sc.stop()
-	print(np.array(sc.storage))
-	np.savetxt('light_readings.txt',np.array(sc.storage),delimiter=',')
+	print(np.array(sc.reading_records))
+	np.savetxt('light_readings_{}.txt'.format(robot_namespace),np.array(sc.reading_records),delimiter=',')
