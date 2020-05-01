7fd47d6f193d82f58599306bf0b9346a2db4e55c
tianpeng <tzhang@g.harvard.edu>
2020-05-01 15:36:59 -0400

    Finished: path planning with FIM gradient ascent, taking constant step size(alpha*gradient=constant) and stop when some mobile sensor's waypoints hit the epsilon circle around the target.

---
+++ src/FIMPathPlanning.py
@@ -0,0 +1,62 @@
+import numpy as np
+def polar_projection(q,ps,r):    
+    """ 
+        Return the projection of a set of points ps onto a circle centered at q with radius r
+    """
+    q=q.reshape(-1,2)
+    ps=ps.reshape(-1,2)
+    dists=np.linalg.norm(ps-q,axis=1)
+
+    dists[dists==0]=1 # Handling the zero distances
+    ps_proj=((ps-q).T/dists * r).T +q
+    return ps_proj
+
+def FIM_ascent_path_planning(f_dLdp,q,ps,n_p,n_timesteps,max_linear_speed,dt,epsilon):
+    """
+        f_dLdp: a function handle, f_dLdp(q,ps)=dLdp.
+        q: Current location of the target.
+        ps: Current locations of the mobile sensors.
+        n_p: The number of sensors
+        n_timesteps: The number of timesteps to plan ahead. 
+        
+            The total time horizon T will be T=n_timesteps*dt.
+        
+        max_linear_speed: the linear speed limit to be set on the mobile sensors.
+        dt: the time differences between two consecutive waypoints.
+        
+            The update step size will be a constant = max_linear_speed * dt
+        
+        epsilon: when the planned trajectories end, how far away should they be to the target.
+        ----------------------------------------------------------------------------------------
+        Output: waypoints for each mobile sensor, Shape= (num_time_steps,num_sensors,2)
+    """
+
+    step_size=max_linear_speed*dt
+    p_trajs=[]
+    
+    q=q.reshape(-1,2)
+    ps=ps.reshape(-1,2)
+    p_trajs.append(ps)
+    for i in range(n_timesteps):
+        # Calculate the gradient
+        grad=f_dLdp(q,ps)
+        grad=grad.reshape(-1,2)
+        grad_sizes=np.linalg.norm(grad,axis=1)
+
+        grad_sizes[grad_sizes==0]=1 # Handle the case where the partial derivative is zero.
+
+        update_steps=(grad.T/grad_sizes * step_size).T # Calculate the update steps to be applied to ps
+
+        candid_ps=ps+update_steps # Calculate the direct update before projecting onto the target sphere.
+
+        # Project candid_ps onto the "surveillance circle" once it steps into it
+        if not np.all(np.linalg.norm(candid_ps-q,axis=1)>=epsilon):
+            insiders=np.linalg.norm(candid_ps-q,axis=1)<epsilon
+            ps=candid_ps
+            ps[insiders]=polar_projection(q,candid_ps,epsilon)[insiders] # Update ps.
+            p_trajs.append(ps)
+            break # Exit the loop once some mobile sensor's trajectory reaches the circle.
+        else:
+            ps=candid_ps # Update ps.
+            p_trajs.append(ps)
+    return np.array(p_trajs).reshape(-1,n_p,2) # Shape= (num_time_steps,num_sensors,2)
