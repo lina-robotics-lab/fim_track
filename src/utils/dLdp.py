080ee007e103db9d59b04ebea66ff57dc37bbcf8
Tianpeng Zhang <tzhang@g.harvard.edu>
2021-02-15 16:04:55 -0500

    Finished: getting rid of jax dependency in gradient calculations, using pure numpy to implement gradient from analytic formulas. The simulation now runs blazingly fast, with 0.8s per 100 iterations(previously it was 80s per 100 iterations). Next: fix the instability issue of consensus filter when the sensors get close to the source.

--- src/utils/dLdp.py
+++ src/utils/dLdp.py
@@ -8,61 +8,44 @@ auto-differentiation.
 
 import numpy as np
 from utils.MutualSepPathPlanning import sep_func
-def analytic_L(q,ps,C1s,C0s,ks,bs,sigma=1):
-    
-    n_p=len(ps)
-    r=jnp.linalg.norm(ps-q,axis=1).reshape(-1,1)
-    r_hat=(ps-q)/r
 
-    L=0
-    for i in range(n_p):
-        for j in range(n_p):
-                
-            rkrj=jnp.min([r_hat[i,:].dot(r_hat[j,:]),1])
-            
-            L+=(bs[i]*bs[j]*ks[i]*ks[j])**2 * (r[i]-C1s[i])**(2*bs[i]-2) * (r[j]-C1s[j])**(2*bs[j]-2) * (1-rkrj**2)
-            
-    L/=2*sigma**2
+
+def dhdr(r,C1s,C0s,ks,bs):
+    return ks*bs*(r-C1s)**(bs-1)
+def d2hdr2(r,C1s,C0s,ks,bs):
+    return dhdr(r,C1s,C0s,ks,bs)*(bs-1)/(r-C1s)
+
+def analytic_dhdz(C1s,C0s,ks,bs,x,ps):
+    q = x[:len(x)//2]
+    rs = np.linalg.norm(ps-q,axis=1)
+   
+    r_hat = ((ps-q).T/rs).T
+    d = dhdr(rs,C1s,C0s,ks,bs)
+    dhdq=-(d * r_hat.T).T
     
-    return L[0]
+    return np.hstack([dhdq,np.zeros(dhdq.shape)])
+    
 def analytic_dLdp(q,ps,C1s,C0s,ks,bs,sigma=1):
     """
         Typically the sigma value is just a scaling on the magnitude of the gradient, so it can take a default value=1.
     """
-    n_p=len(ps)
-    r=np.linalg.norm(ps-q,axis=1).reshape(-1,1)
-    r_hat=(ps-q)/r
+  
+    rs = np.linalg.norm(ps-q,axis=1)
+    r_hat = ((ps-q).T/rs).T
     t_hat=np.zeros(r_hat.shape)
     t_hat[:,0]=-r_hat[:,1]
     t_hat[:,1]=r_hat[:,0]
 
-    dLdeta=np.zeros(n_p).reshape(-1,1)
-    dLdr=np.zeros(n_p).reshape(-1,1)
+    d = dhdr(rs,C1s,C0s,ks,bs)
+    dd = d2hdr2(rs,C1s,C0s,ks,bs)
 
+    wrhat=(d*r_hat.T).T
+    Q = np.linalg.inv(wrhat.T.dot(wrhat)) # FIM^-1
 
-    for i in range(n_p):
-        Keta=2*(ks[i]*bs[i])**2/(sigma**2) * (r[i]-C1s[i])**(2*bs[i]-2)
-        Kr=2*(ks[i]*bs[i])**2/(sigma**2) * (bs[i]-1) * (r[i]-C1s[i])**(2*bs[i]-3)
-        sum_eta=sum_kr=0
-        for j in range(n_p):
-                
-            rkrj=np.max([np.min([r_hat[i,:].dot(r_hat[j,:]),1]),-1])
-            
-            direction=np.sign(np.linalg.det(r_hat[[j,i],:]))
+    c1 = 2*d*dd*np.linalg.norm(Q.dot(r_hat.T),axis=0)**2
+    c2 = 2*(1/rs)*(d**2)*np.einsum('ij,ij->j',Q.dot(r_hat.T),Q.dot(t_hat.T))
 
-            sum_eta += (ks[j]*bs[j])**2 * (r[j]-C1s[j])**(2*bs[j]-2) * rkrj * np.sqrt(1-rkrj**2) * direction
-            sum_kr += (ks[j]*bs[j])**2 * (r[j]-C1s[j])**(2*bs[j]-2) * (1-rkrj**2)
-        
-        dLdeta[i]=Keta*sum_eta
-        dLdr[i]=Kr*sum_kr
-        
-    dLdp = dLdr * r_hat  + (dLdeta/r) * t_hat
-    
-    
-    return dLdp
-
-
-#### Temporaily Freeze the jax-dependent part. The analytic version has less dependency thus more compatibility.###################################################################
+    return (c1*r_hat.T+c2*t_hat.T).T
 
 
 from jax import grad,jit, jacfwd
